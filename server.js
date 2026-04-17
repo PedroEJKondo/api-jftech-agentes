844d56b473400e1bc7cbd045c15588e032eb6e72
Pedro Kondo <pedrokondo20@gmail.com>
2026-04-17 03:13:59 +0100

    chore ver ficheiros

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.use(express.json());
+
+// servir arquivos estáticos da pasta files
+app.use('/files', express.static(path.join(__dirname, 'files')));
 
 // importar rotas
 const agentesRoutes = require('./routes/agentes');
